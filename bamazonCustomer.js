e3d4eb35c5b83ab2641f79f88f2ff427830af660
code3lue <alan.delez@gmail.com>
2017-04-24 22:23:29 -0400

    need to finalize purchases

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,12 +15,13 @@ connection.connect(function(err){
     
 });
 
+
 var questions = [{
     name: 'chooseID',
     type: 'input',
     message: 'Choose an item to buy by ID',
     validate: function (value) {
-        if (isNaN(value)== false && value < 10 && value > 0 && value !== undefined){
+        if (isNaN(value)== false && value <= 10 && value > 0 && value !== undefined){
             
       
        return true
@@ -51,22 +52,6 @@ var questions = [{
         }
 
 }];
-
-/*function checkItem() {
-    
-    console.log(id)
-    console.log(quantity)
-    
-    connection.query('select * products', function (err, data) {
-        if (err) throw err;
-        for (var i = 0; i < data.length; i++) {
-
-        }
-
-    })
-}*/
-
-
 function getItems() {
         
     connection.query('select * from products', function (err, data) {
@@ -93,33 +78,58 @@ inquirer.prompt(questions).then(function(answer) {
    var quant = answer.quantity;
      //query to find the quantity of the item they selected
 
-connection.query('select stock_quantity, item_id from products', function(err, data) {
+connection.query('select item_id, stock_quantity from products', function(err, data) {
     if (err) throw err;
-    console.log(id)
+    //console.log(id)
     var string = JSON.stringify(data);
     var obj = JSON.parse(string);
-    console.log(obj)
+   // console.log(obj)
+   var arrayID = (id - 1)
+    //console.log(arrayID)
+     for (var i = 0; i < obj.length; i++) {
+        if (i === arrayID) {
+           // console.log(obj[i].stock_quantity);
+            //console.log(quant)
+            if (quant <= obj[i].stock_quantity && quant > 0) {
+                
+           
+                function purchase() {
+                    
+                    var subtract = (obj[i].stock_quantity - quant)
+                    console.log(subtract)
+            
 
-    //**********************THIS IS WHERE YOU LEFT OFF OBJ IS AN ARRAY */
-   
+                    connection.query('update products set ? where ?'), [{
+                        stock_quantity: subtract}, {
+                            item_id: obj[i].stock_quantity
+                        }], function(error) {
+                            if (error) throw error;
+                            console.log("Purchase Successful!");
+                        };
+                  
+                };
+                purchase()
+            };
+            if (obj[i].stock_quantity === 0){
+                console.log("Item OUT OF STOCK")
+                start();
+            };
+
+                console.log('Sorry, please enter valid quantity')
+                start();
+            
+            };
+        };
+    
+     });
 
     //JSON.stringify gets rid of that pesky ROWDATAPACKET
-   
-    
-    
-})
-
-
-                    //query to find the quantity of the item they selected
+});
+}
+   //query to find the quantity of the item they selected
                 //make sure the user quantity is less than the item quantity
                 //if yes, display price
-                //if no, display 'you are loser. you can't have that'
-
-});   
-};
-               
-               
-
+                //if no, display 'you are loser. you can't have that'            
 getItems();
 
 
